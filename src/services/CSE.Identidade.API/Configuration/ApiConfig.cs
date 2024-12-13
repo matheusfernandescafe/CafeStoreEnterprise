33e1ad862ae3c32f1d9a3b058a64394bad257684
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2024-12-13 08:37:55 -0300

    feat: Configurando Program

---
+++ src/services/CSE.Identidade.API/Configuration/ApiConfig.cs
@@ -0,0 +1,25 @@
+﻿namespace CSE.Identidade.API.Configuration;
+
+public static class ApiConfig
+{
+    public static WebApplicationBuilder AddApiConfig(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers()
+            .ConfigureApiBehaviorOptions(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
+        return builder;
+    }
+
+    public static WebApplication UseApiConfig(this WebApplication app)
+    {
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        return app;
+    }
+}
---
+++ src/services/CSE.Identidade.API/Configuration/DbContextConfig.cs
@@ -0,0 +1,17 @@
+﻿using CSE.Identidade.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSE.Identidade.API.Configuration;
+
+public static class DbContextConfig
+{
+    public static WebApplicationBuilder AddDbContextConfig(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        return builder;
+    }
+}
