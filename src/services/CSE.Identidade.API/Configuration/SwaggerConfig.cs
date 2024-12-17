673c9026195215658ca7f0a6acd0e054ee8d6eb1
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2024-12-17 07:15:23 -0300

    style: Adicionado documentação no Swagger

--- src/services/CSE.Identidade.API/Configuration/SwaggerConfig.cs
+++ src/services/CSE.Identidade.API/Configuration/SwaggerConfig.cs
@@ -9,6 +9,14 @@ public static class SwaggerConfig
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen(c =>
         {
+            c.SwaggerDoc("v1", new OpenApiInfo()
+            {
+                Title = "CafeStore Enterprise Identity API",
+                Description = "API desenvolvida durante o curso \"ASP.NET Core Enterprise Applications\" da plataforma Desenvolvedor.io. Este repositório demonstra práticas avançadas na construção de aplicações empresariais com o ASP.NET Core.",
+                Contact = new OpenApiContact() { Name = "Matheus Fernandes", Email = "matheusfernandesofc@gmail.com" },
+                License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+            });
+
             c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
             {
                 Description = "Insira o token JWT desta maneira: Bearer {seu token}",
