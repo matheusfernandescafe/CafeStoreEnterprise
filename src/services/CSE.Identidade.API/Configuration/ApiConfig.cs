a2c9b04e3977e7324b462d512404df1d25bd9875
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-01-15 17:19:48 -0300

    feat(ApiConfig): Configuração de ambiente e ajustes em appsettings
    Adição de configuração de ambiente no método `AddApiConfig` da classe `ApiConfig`, que agora carrega arquivos de configuração JSON específicos do ambiente e variáveis de ambiente. Se o ambiente for de desenvolvimento, os segredos do usuário são adicionados à configuração.
    
    No arquivo `appsettings.Development.json`, foram adicionadas configurações de `AllowedHosts`, `ConnectionStrings` e `AppSettings`, incluindo a string de conexão padrão e várias configurações de aplicativo.
    
    No arquivo `appsettings.json`, a seção `ConnectionStrings` foi removida, sugerindo que essa configuração será gerenciada apenas no arquivo de configuração específico do ambiente de desenvolvimento.

--- src/services/CSE.Identidade.API/Configuration/ApiConfig.cs
+++ src/services/CSE.Identidade.API/Configuration/ApiConfig.cs
@@ -4,6 +4,19 @@ public static class ApiConfig
 {
     public static WebApplicationBuilder AddApiConfig(this WebApplicationBuilder builder)
     {
+        var environmentName = builder.Environment.EnvironmentName;
+
+        builder.Configuration
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.Configuration.AddUserSecrets<Program>();
+        }
+
         builder.Services.AddControllers()
             .ConfigureApiBehaviorOptions(options =>
             {
