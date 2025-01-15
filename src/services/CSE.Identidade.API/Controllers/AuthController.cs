cd58fbbdbc04bb8e30fdce994b3df30bf2e0fa7c
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-01-15 16:48:32 -0300

    Refatora AuthController para usar CustomResponse
    - AuthController agora herda de MainController em vez de Controller.
    - Método Registrar usa CustomResponse para respostas de erro e sucesso.
    - Método Login usa CustomResponse para respostas de erro e sucesso.
    - Adiciona mensagens de erro ao CustomResponse em casos de falha no Registrar e Login.

--- src/services/CSE.Identidade.API/Controllers/AuthController.cs
+++ src/services/CSE.Identidade.API/Controllers/AuthController.cs
@@ -14,7 +14,7 @@ namespace CSE.Identidade.API.Controllers;
 [Route("api/identidade")]
 public class AuthController(SignInManager<IdentityUser> signInManager,
                       UserManager<IdentityUser> userManager,
-                      IOptions<AppSettings> appSettings) : Controller
+                      IOptions<AppSettings> appSettings) : MainController
 {
     private readonly SignInManager<IdentityUser> _signInManager = signInManager;
     private readonly UserManager<IdentityUser> _userManager = userManager;
@@ -23,7 +23,7 @@ public class AuthController(SignInManager<IdentityUser> signInManager,
     [HttpPost("nova-conta")]
     public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
     {
-        if (!ModelState.IsValid) return BadRequest(ModelState);
+        if (!ModelState.IsValid) return CustomResponse(ModelState);
 
         var user = new IdentityUser
         {
@@ -36,27 +36,39 @@ public class AuthController(SignInManager<IdentityUser> signInManager,
 
         if (result.Succeeded)
         {
-            await _signInManager.SignInAsync(user, isPersistent: false);
-            return Ok(await GerarJwt(usuarioRegistro.Email));
+            return CustomResponse(await GerarJwt(usuarioRegistro.Email));
         }
 
-        return BadRequest();
+        foreach (var error in result.Errors)
+        {
+            AdicionarErroProcessamento(error.Description);
+        }
+
+        return CustomResponse();
     }
 
     [HttpPost("autenticar")]
     public async Task<ActionResult> Login(UsuarioLogin usuarioLogin)
     {
-        if (!ModelState.IsValid) return BadRequest(ModelState);
+        if (!ModelState.IsValid) return CustomResponse(ModelState);
 
         var result = await _signInManager.PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Senha,
             false, true);
 
         if (result.Succeeded)
         {
-            return Ok(await GerarJwt(usuarioLogin.Email));
+            return CustomResponse(await GerarJwt(usuarioLogin.Email));
         }
 
-        return BadRequest();
+        if (result.IsLockedOut)
+        {
+            AdicionarErroProcessamento("Usuário temporariamente bloqueado por tentativas inválidas");
+            return CustomResponse();
+        }
+
+        AdicionarErroProcessamento("Usuário ou Senha incorretos");
+
+        return CustomResponse();
     }
 
     private async Task<UsuarioRespostaLogin> GerarJwt(string email)
