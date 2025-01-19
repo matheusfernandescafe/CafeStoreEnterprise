54a4d36e202e08848334d1c5460feeec3f07cd48
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-01-19 20:08:26 -0300

    feat(AutenticacaoService): Adiciona serviço de autenticação com login e registro

---
+++ src/web/CSE.WebApp.MVC/Services/AutenticacaoService.cs
@@ -0,0 +1,44 @@
+﻿using CSE.WebApp.MVC.Models;
+using System.Text;
+using System.Text.Json;
+
+namespace CSE.WebApp.MVC.Services;
+
+public class AutenticacaoService(HttpClient httpClient) : IAutenticacaoService
+{
+    private readonly HttpClient _httpClient = httpClient;
+
+    public async Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin)
+    {
+        var loginContent = new StringContent(
+            JsonSerializer.Serialize(usuarioLogin),
+            Encoding.UTF8,
+            "application/json");
+
+        var response = await _httpClient.PostAsync("https://localhost:5001/api/identidade/autenticar", loginContent);
+
+        var responseSeralizer = JsonSerializer.Deserialize<UsuarioRespostaLogin>(await response.Content.ReadAsStringAsync(), new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        return responseSeralizer!;
+    }
+
+    public async Task<UsuarioRespostaLogin> Registro(UsuarioRegistro usuarioRegistro)
+    {
+        var loginContent = new StringContent(
+            JsonSerializer.Serialize(usuarioRegistro),
+            Encoding.UTF8,
+            "application/json");
+
+        var response = await _httpClient.PostAsync("https://localhost:5001/api/identidade/nova-conta", loginContent);
+
+        var responseSeralizer = JsonSerializer.Deserialize<UsuarioRespostaLogin>(await response.Content.ReadAsStringAsync(), new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        return responseSeralizer!;
+    }
+}
---
+++ src/web/CSE.WebApp.MVC/Services/IAutenticacaoService.cs
@@ -0,0 +1,9 @@
+﻿using CSE.WebApp.MVC.Models;
+
+namespace CSE.WebApp.MVC.Services;
+
+public interface IAutenticacaoService
+{
+    Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin);
+    Task<UsuarioRespostaLogin> Registro(UsuarioRegistro usuarioRegistro);
+}
