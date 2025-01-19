dbd4166aa8c00f76918a91df3ee45d1b61249deb
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-01-19 18:13:34 -0300

    feat: Adiciona classes para registro e login de usuários
    Foram adicionadas novas classes ao arquivo `UsuarioViewModel.cs` para gerenciar o registro e login de usuários, bem como a resposta de login e os tokens de usuário.

---
+++ src/web/CSE.WebApp.MVC/Models/UsuarioViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace CSE.WebApp.MVC.Models;
+
+public class UsuarioRegistro
+{
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+    public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+    public string Senha { get; set; } = null!;
+
+    [DisplayName("Confirme sua senha")]
+    [Compare("Senha", ErrorMessage = "As senhas não conferem.")]
+    public string SenhaConfirmacao { get; set; } = null!;
+}
+
+public class UsuarioLogin
+{
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+    public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+    public string Senha { get; set; } = null!;
+}
+
+public class UsuarioRespostaLogin
+{
+    public string AccessToken { get; set; } = null!;
+    public double ExpiresIn { get; set; }
+    public UsuarioToken UsuarioToken { get; set; } = new();
+    //public ResponseResult ResponseResult { get; set; } = new();
+}
+
+public class UsuarioToken
+{
+    public string Id { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public IEnumerable<UsuarioClaim> Claims { get; set; } = [];
+}
+
+public class UsuarioClaim
+{
+    public string Value { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+}
