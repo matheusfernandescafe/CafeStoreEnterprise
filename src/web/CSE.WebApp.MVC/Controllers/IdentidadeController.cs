721181dd435c8f185fb0a4145e38a8792ff39eb2
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-01-19 20:09:20 -0300

    feat(IdentidadeController): Adiciona IdentidadeController e métodos de autenticação

---
+++ src/web/CSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -0,0 +1,51 @@
+﻿using CSE.WebApp.MVC.Models;
+using CSE.WebApp.MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSE.WebApp.MVC.Controllers;
+
+public class IdentidadeController(IAutenticacaoService autenticacaoService) : Controller
+{
+    private readonly IAutenticacaoService _iAutenticacaoService = autenticacaoService;
+
+    [HttpGet("nova-conta")]
+    public IActionResult Registro()
+    {
+        return View();
+    }
+    
+    [HttpPost("nova-conta")]
+    public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+    {
+        if (!ModelState.IsValid) return View(usuarioRegistro);
+
+        var resposta = await _iAutenticacaoService.Registro(usuarioRegistro);
+
+        return View(usuarioRegistro);
+    }
+    
+    [HttpGet("login")]
+    public IActionResult Login()
+    {
+        return View();
+    }
+    
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+    {
+        if (!ModelState.IsValid) return View(usuarioLogin);
+
+        var resposta = await _iAutenticacaoService.Login(usuarioLogin);
+
+        if (false) return View(usuarioLogin);
+
+        return RedirectToAction("Index", "Home");
+    }
+    
+    [HttpGet("sair")]
+    public async Task<IActionResult> Logout()
+    {
+        return RedirectToAction("Index", "Home");
+    }
+
+}
