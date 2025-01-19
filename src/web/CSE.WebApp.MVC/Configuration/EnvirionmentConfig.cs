a42ba89a84e4c7328b96f9e9972a521a394c42fc
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-01-19 20:07:02 -0300

    refactor(Program): Refatora configuração de ambiente e simplifica Program.cs

---
+++ src/web/CSE.WebApp.MVC/Configuration/EnvirionmentConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace CSE.WebApp.MVC.Configuration;
+
+public static class EnvirionmentConfig
+{
+    public static void AddEnvironmentConfig(this WebApplicationBuilder builder)
+    {
+        var environment = builder.Environment;
+        builder.Configuration
+            .SetBasePath(environment.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        if (environment.IsDevelopment())
+        {
+            builder.Configuration.AddUserSecrets<Program>();
+        }
+    }
+}
--- src/web/CSE.WebApp.MVC/Program.cs
+++ src/web/CSE.WebApp.MVC/Program.cs
@@ -2,21 +2,10 @@ using CSE.WebApp.MVC.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var environment = builder.Environment;
-builder.Configuration
-    .SetBasePath(environment.ContentRootPath)
-    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-    .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
-    .AddEnvironmentVariables();
+builder.AddEnvironmentConfig();
 
-if (environment.IsDevelopment())
-{
-    builder.Configuration.AddUserSecrets<Program>();
-}
-
-var services = builder.Services;
-services.AddIdentityConfiguration();
-services.AddMvcConfiguration(builder.Configuration);
+builder.Services.AddIdentityConfiguration();
+builder.Services.AddMvcConfiguration(builder.Configuration);
 
 var app = builder.Build();
 
