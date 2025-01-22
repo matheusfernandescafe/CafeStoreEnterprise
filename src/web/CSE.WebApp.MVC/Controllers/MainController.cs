e37f1deec6f27f4a2caf46d2ce2c2ffed6a02d8a
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-01-21 22:14:03 -0300

    feat(MainController): Adiciona MainController e método de verificação de erros
    `ResponsePossuiErros` na classe `MainController`, que verifica se a resposta possui erros e, se sim, adiciona esses erros ao `ModelState` e retorna `true`. Caso contrário, retorna `false`.

---
+++ src/web/CSE.WebApp.MVC/Controllers/MainController.cs
@@ -0,0 +1,22 @@
+﻿using CSE.WebApp.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSE.WebApp.MVC.Controllers;
+
+public class MainController : Controller
+{
+    protected bool ResponsePossuiErros(ResponseResult resposta)
+    {
+        if (resposta != null && resposta.Errors.Mensagens.Count != 0)
+        {
+            foreach (var mensagem in resposta.Errors.Mensagens)
+            {
+                ModelState.AddModelError(string.Empty, mensagem);
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+}
