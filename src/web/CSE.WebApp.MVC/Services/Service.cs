67bb0bd4f79e7fc9bdfaeb185876bcadfcb3755e
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-01-21 22:22:39 -0300

    feat(Service): Adiciona classe Service com tratamento de erros HTTP

---
+++ src/web/CSE.WebApp.MVC/Services/Service.cs
@@ -0,0 +1,24 @@
+﻿using CSE.WebApp.MVC.Extensions;
+
+namespace CSE.WebApp.MVC.Services;
+
+public abstract class Service
+{
+    protected bool TratarErrosResponse(HttpResponseMessage response)
+    {
+        switch ((int)response.StatusCode)
+        {
+            case 401:
+            case 403:
+            case 404:
+            case 500:
+                throw new CustomHttpRequestException(response.StatusCode);
+
+            case 400:
+                return false;
+        }
+
+        response.EnsureSuccessStatusCode();
+        return true;
+    }
+}
