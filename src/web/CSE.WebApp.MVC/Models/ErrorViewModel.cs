691e119b88427d6f77e657b03330bcb81eb47aea
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-01-21 22:16:44 -0300

    feat(ErrorViewModeal): Adiciona novas classes ao ErrorViewModel.cs
    Foram adicionadas três novas classes ao arquivo `ErrorViewModel.cs` dentro do namespace

--- src/web/CSE.WebApp.MVC/Models/ErrorViewModel.cs
+++ src/web/CSE.WebApp.MVC/Models/ErrorViewModel.cs
@@ -1,9 +1,27 @@
-namespace CSE.WebApp.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace CSE.WebApp.MVC.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
+public class ErrorViewModel2
+{
+    public int ErroCode { get; set; }
+    public string Titulo { get; set; } = string.Empty;
+    public string Mensagem { get; set; } = string.Empty;
+}
+
+public class ResponseResult
+{
+    public string Title { get; set; } = string.Empty;
+    public int Status { get; set; }
+    public ResponseErrorMessages Errors { get; set; } = new();
+}
+
+public class ResponseErrorMessages
+{
+    public List<string> Mensagens { get; set; } = [];
 }
