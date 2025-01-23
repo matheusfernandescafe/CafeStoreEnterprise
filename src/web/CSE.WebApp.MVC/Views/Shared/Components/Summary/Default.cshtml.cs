ce86799002ffaa75a845065bdbaae9e1b3cd7fc7
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-01-23 20:52:25 -0300

    feat(Default): Adiciona tratamento de erros no ModelState
    No arquivo `Default.cshtml`, foi adicionado um bloco de código que verifica se há erros no `ModelState` e, em caso afirmativo, exibe uma mensagem de alerta com um sumário de validação.

---
+++ src/web/CSE.WebApp.MVC/Views/Shared/Components/Summary/Default.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSE.WebApp.MVC.Views.Components.Summary
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
