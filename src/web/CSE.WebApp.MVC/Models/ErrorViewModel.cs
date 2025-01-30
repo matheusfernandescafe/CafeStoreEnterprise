5524b3e9a3bacb7972171e59a512e1642ca7adf4
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-01-29 22:24:51 -0300

    refactor(ErrorViewModel): Remove e adiciona propriedades
    A classe `ErrorViewModel` foi alterada para remover as propriedades `RequestId` e `ShowRequestId`.
    
    Foram adicionadas três novas propriedades à classe `ErrorViewModel`:
    - `ErroCode` (do tipo `int`)
    - `Titulo` (do tipo `string` com valor padrão `string.Empty`)
    - `Mensagem` (do tipo `string` com valor padrão `string.Empty`)

--- src/web/CSE.WebApp.MVC/Models/ErrorViewModel.cs
+++ src/web/CSE.WebApp.MVC/Models/ErrorViewModel.cs
@@ -2,9 +2,9 @@ namespace CSE.WebApp.MVC.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    public int ErroCode { get; set; }
+    public string Titulo { get; set; } = string.Empty;
+    public string Mensagem { get; set; } = string.Empty;
 }
 
 public class ErrorViewModel2
