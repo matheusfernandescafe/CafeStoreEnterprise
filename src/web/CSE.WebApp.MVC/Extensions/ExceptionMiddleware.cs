0eb7eb0b070932bbaa4405e0d5805e8e8965c220
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-01-29 22:21:22 -0300

    feat(ExceptionMiddleware): Implementação de middleware de exceção personalizado
    Implementado o método `HandleRequestExceptionAsync` que trata exceções do tipo `CustomHttpRequestException`. Se o status da exceção for `Unauthorized`, o usuário é redirecionado para a página de login com a URL de retorno. Caso contrário, o código de status da resposta é definido com o código da exceção.

---
+++ src/web/CSE.WebApp.MVC/Extensions/ExceptionMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+
+namespace CSE.WebApp.MVC.Extensions;
+
+public class ExceptionMiddleware(RequestDelegate next)
+{
+    private readonly RequestDelegate _next = next;
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (CustomHttpRequestException ex)
+        {
+            HandleRequestExceptionAsync(httpContext, ex);
+        }
+    }
+
+    private static void HandleRequestExceptionAsync(HttpContext context, CustomHttpRequestException httpRequestException)
+    {
+        if (httpRequestException.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            context.Response.Redirect($"/login?ReturnUrl={context.Request.Path}");
+            return;
+        }
+
+        context.Response.StatusCode = (int)httpRequestException.StatusCode;
+    }
+}
