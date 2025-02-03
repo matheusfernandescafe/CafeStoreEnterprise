368839c5dbeca3e87b71bbeb67cf15b0cf199d1d
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-02-03 20:15:12 -0300

    refactor(AutenticacaoService): Refatoração de AutenticacaoService para usar AppSettings

---
+++ src/web/CSE.WebApp.MVC/Extensions/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace CSE.WebApp.MVC.Extensions;
+
+public class AppSettings
+{
+    public string AutenticacaoUrl { get; set; } = null!;
+    public string CatalogoUrl { get; set; } = null!;
+}
--- src/web/CSE.WebApp.MVC/Services/AutenticacaoService.cs
+++ src/web/CSE.WebApp.MVC/Services/AutenticacaoService.cs
@@ -1,16 +1,20 @@
-﻿using CSE.WebApp.MVC.Models;
+﻿using CSE.WebApp.MVC.Extensions;
+using CSE.WebApp.MVC.Models;
+using Microsoft.Extensions.Options;
 
 namespace CSE.WebApp.MVC.Services;
 
-public class AutenticacaoService(HttpClient httpClient) : Service, IAutenticacaoService
+public class AutenticacaoService(HttpClient httpClient,
+    IOptions<AppSettings> settings) : Service, IAutenticacaoService
 {
     private readonly HttpClient _httpClient = httpClient;
+    private readonly AppSettings _settings = settings.Value;
 
     public async Task<UsuarioRespostaLogin?> Login(UsuarioLogin usuarioLogin)
     {
         var loginContent = ObterConteudo(usuarioLogin);
 
-        var response = await _httpClient.PostAsync("/api/identidade/autenticar", loginContent);
+        var response = await _httpClient.PostAsync($"{_settings.AutenticacaoUrl}/api/identidade/autenticar", loginContent);
 
         if (!TratarErrosResponse(response))
         {
@@ -27,7 +31,7 @@ public class AutenticacaoService(HttpClient httpClient) : Service, IAutenticacao
     {
         var registroContent = ObterConteudo(usuarioRegistro);
 
-        var response = await _httpClient.PostAsync("/api/identidade/nova-conta", registroContent);
+        var response = await _httpClient.PostAsync($"{_settings.AutenticacaoUrl}/api/identidade/nova-conta", registroContent);
 
         if (!TratarErrosResponse(response))
         {
