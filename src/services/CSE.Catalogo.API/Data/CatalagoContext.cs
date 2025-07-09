ca22f73301c015d89ef20fa41b0c3c863a179426
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-08 21:03:11 -0300

    feat: Criado criado context e primeira migration

---
+++ src/services/CSE.Catalogo.API/Data/CatalagoContext.cs
@@ -0,0 +1,24 @@
+﻿using CSE.Catalogo.API.Models;
+using CSE.Core.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSE.Catalogo.API.Data;
+
+public class CatalogoContext(DbContextOptions<CatalogoContext> options) : DbContext(options), IUnitOfWork
+{
+    public DbSet<Produto> Produtos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+            e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+            property.SetColumnType("varchar(100)");
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoContext).Assembly);
+    }
+
+    public async Task<bool> Commit()
+    {
+        return await base.SaveChangesAsync() > 0;
+    }
+}
