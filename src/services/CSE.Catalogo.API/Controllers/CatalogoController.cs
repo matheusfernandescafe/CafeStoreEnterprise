2bf8b20619dcc4418f07d1af5b41d075ef0cd2e8
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-08 21:27:34 -0300

    feat: Criado CatalogoController com métodos para obter todos os produtos e detalhes de um produto específico, utilizando a interface `IProdutoRepository`.

---
+++ src/services/CSE.Catalogo.API/Controllers/CatalogoController.cs
@@ -0,0 +1,20 @@
+﻿using CSE.Catalogo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSE.Catalogo.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+
+public class CatalogoController(IProdutoRepository productRepository) : Controller
+{
+    private readonly IProdutoRepository _iProdutoRepository = productRepository;
+
+    [HttpGet("Produtos")]
+    public async Task<IEnumerable<Produto>> Index()
+        => await _iProdutoRepository.ObterTodos();
+
+    [HttpGet("Produtos/{id}")]
+    public async Task<Produto?> ProdutoDetalhe(Guid id)
+        => await _iProdutoRepository.ObterPorId(id);
+}
