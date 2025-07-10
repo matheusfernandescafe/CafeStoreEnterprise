651adb27f8e3d679fcc70332e743aa7b831f7727
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-09 21:57:06 -0300

    Adicionado script .sql para popular banco de Produtos

---
+++ src/services/CSE.Catalogo.API/Data/ProdutosSeed.sql
@@ -0,0 +1,11 @@
+USE [CafeStoreEnterprise]
+GO
+
+INSERT [dbo].[Produtos] ([Id], [Nome], [Descricao], [Ativo], [Valor], [DataCadastro], [Imagem], [QuantidadeEstoque]) VALUES (NEWID(), 'Camiseta Code Life Preta', 'Camiseta 100% algodão, resistente a lavagens e altas temperaturas.', 1, 90.00, GETDATE(), 'camiseta2.jpg', 100)
+INSERT [dbo].[Produtos] ([Id], [Nome], [Descricao], [Ativo], [Valor], [DataCadastro], [Imagem], [QuantidadeEstoque]) VALUES (NEWID(), 'Caneca No Coffee No Code', 'Caneca de porcelana com impressão térmica de alta resistência.', 1, 50.00, GETDATE(), 'caneca4.jpg', 100)
+INSERT [dbo].[Produtos] ([Id], [Nome], [Descricao], [Ativo], [Valor], [DataCadastro], [Imagem], [QuantidadeEstoque]) VALUES (NEWID(), 'Camiseta Debugar Preta', 'Camiseta 100% algodão, resistente a lavagens e altas temperaturas.', 1, 75.00, GETDATE(), 'camiseta4.jpg', 150)
+INSERT [dbo].[Produtos] ([Id], [Nome], [Descricao], [Ativo], [Valor], [DataCadastro], [Imagem], [QuantidadeEstoque]) VALUES (NEWID(), 'Camiseta Code Life Cinza', 'Camiseta 100% algodão, resistente a lavagens e altas temperaturas.', 1, 80.00, GETDATE(), 'camiseta3.jpg', 7)
+INSERT [dbo].[Produtos] ([Id], [Nome], [Descricao], [Ativo], [Valor], [DataCadastro], [Imagem], [QuantidadeEstoque]) VALUES (NEWID(), 'Caneca Star Bugs Coffee', 'Caneca de porcelana com impressão térmica de alta resistência.', 1, 20.00, GETDATE(), 'caneca1.jpg', 0)
+INSERT [dbo].[Produtos] ([Id], [Nome], [Descricao], [Ativo], [Valor], [DataCadastro], [Imagem], [QuantidadeEstoque]) VALUES (NEWID(), 'Caneca Programmer Code', 'Caneca de porcelana com impressão térmica de alta resistência.', 1, 15.00, GETDATE(), 'caneca2.jpg', 1)
+INSERT [dbo].[Produtos] ([Id], [Nome], [Descricao], [Ativo], [Valor], [DataCadastro], [Imagem], [QuantidadeEstoque]) VALUES (NEWID(), 'Camiseta Software Developer', 'Camiseta 100% algodão, resistente a lavagens e altas temperaturas.', 1, 100.00, GETDATE(), 'camiseta1.jpg', 9)
+INSERT [dbo].[Produtos] ([Id], [Nome], [Descricao], [Ativo], [Valor], [DataCadastro], [Imagem], [QuantidadeEstoque]) VALUES (NEWID(), 'Caneca Turn Coffee in Code', 'Caneca de porcelana com impressão térmica de alta resistência.', 1, 20.00, GETDATE(), 'caneca3.jpg', 0)
