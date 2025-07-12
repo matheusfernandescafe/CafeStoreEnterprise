256a13aed4a4f1372dc21b3ae5d904071f114490
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-12 16:45:08 -0300

    feat(WebAPI.Core): Criado projeto paraunificar implementações "core" em comum para todas as APIs

---
+++ src/building blocks/CSE.WebAPI.Core/Identidade/JwtConfig.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace CSE.WebAPI.Core.Identidade;
+
+public static class JwtConfig
+{
+    public static WebApplicationBuilder AddJwtConfiguration(this WebApplicationBuilder builder)
+    {
+        var JwtSettingsSection = builder.Configuration.GetSection("AppSettings");
+        builder.Services.Configure<AppSettings>(JwtSettingsSection);
+
+        var jwtSettings = JwtSettingsSection.Get<AppSettings>();
+        var key = Encoding.ASCII.GetBytes(jwtSettings.Secret);
+
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.RequireHttpsMetadata = true;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = jwtSettings.ValidoEm,
+                ValidIssuer = jwtSettings.Emissor
+            };
+        });
+
+        return builder;
+    }
+
+    public static WebApplication UseAuthConfiguration(this WebApplication app)
+    {
+        app.UseAuthentication();
+        app.UseAuthorization();
+        return app;
+    }
+}
