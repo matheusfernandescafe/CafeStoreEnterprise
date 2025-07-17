aafef489025276a50e43dfffecff6027e30d5d3c
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-16 23:08:51 -0300

    feat(Catalago): Adicionar controle de acesso no CatalogoController
    O método `ProdutoDetalhe` agora requer a permissão `[ClaimsAuthorize("Catalogo", "Ler")]`.
    O método `Index` foi modificado para permitir acesso anônimo com `[AllowAnonymous]`.

--- src/services/CSE.Catalogo.API/Controllers/CatalogoController.cs
+++ src/services/CSE.Catalogo.API/Controllers/CatalogoController.cs
@@ -1,19 +1,24 @@
 ﻿using CSE.Catalogo.API.Models;
+using CSE.WebAPI.Core.Identidade;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CSE.Catalogo.API.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
+[Authorize]
 
 public class CatalogoController(IProdutoRepository productRepository) : Controller
 {
     private readonly IProdutoRepository _iProdutoRepository = productRepository;
 
+    [AllowAnonymous]
     [HttpGet("Produtos")]
     public async Task<IEnumerable<Produto>> Index()
         => await _iProdutoRepository.ObterTodos();
 
+    [ClaimsAuthorize("Catalago", "Ler")]
     [HttpGet("Produtos/{id}")]
     public async Task<Produto?> ProdutoDetalhe(Guid id)
         => await _iProdutoRepository.ObterPorId(id);
