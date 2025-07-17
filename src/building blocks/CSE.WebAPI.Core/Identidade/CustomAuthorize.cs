0e98c575674212837fdd10bc79594eaf128cc153
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-16 23:09:16 -0300

    refactor(CustomAuthorize): Melhora a segurança e a sintaxe em CustomAuthorize.cs

--- src/building blocks/CSE.WebAPI.Core/Identidade/CustomAuthorize.cs
+++ src/building blocks/CSE.WebAPI.Core/Identidade/CustomAuthorize.cs
@@ -9,32 +9,26 @@ public class CustomAuthorization
 {
     public static bool ValidarClaimsUsuario(HttpContext context, string claimName, string claimValue)
     {
-        return context.User.Identity.IsAuthenticated &&
+        return context.User?.Identity?.IsAuthenticated == true &&
                context.User.Claims.Any(c => c.Type == claimName && c.Value.Contains(claimValue));
     }
-
 }
 
 public class ClaimsAuthorizeAttribute : TypeFilterAttribute
 {
     public ClaimsAuthorizeAttribute(string claimName, string claimValue) : base(typeof(RequisitoClaimFilter))
     {
-        Arguments = new object[] { new Claim(claimName, claimValue) };
+        Arguments = [new Claim(claimName, claimValue)];
     }
 }
 
-public class RequisitoClaimFilter : IAuthorizationFilter
+public class RequisitoClaimFilter(Claim claim) : IAuthorizationFilter
 {
-    private readonly Claim _claim;
-
-    public RequisitoClaimFilter(Claim claim)
-    {
-        _claim = claim;
-    }
+    private readonly Claim _claim = claim;
 
     public void OnAuthorization(AuthorizationFilterContext context)
     {
-        if (!context.HttpContext.User.Identity.IsAuthenticated)
+        if (context.HttpContext.User?.Identity?.IsAuthenticated != true)
         {
             context.Result = new StatusCodeResult(401);
             return;
