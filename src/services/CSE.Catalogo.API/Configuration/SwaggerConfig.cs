d789f84591ec13a107864654546192c3f1071fbd
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-16 23:15:24 -0300

    feat: Adicionar autenticação JWT e melhorias no Swagger UI
    Implementadas definições de segurança para autenticação via token JWT, incluindo o esquema "Bearer" e instruções para inserção do token no cabeçalho. Adicionada exigência de segurança que referencia o esquema "Bearer". Melhorias na configuração do Swagger UI para exibir a duração da requisição e habilitar filtro.

--- src/services/CSE.Catalogo.API/Configuration/SwaggerConfig.cs
+++ src/services/CSE.Catalogo.API/Configuration/SwaggerConfig.cs
@@ -16,6 +16,31 @@ public static class SwaggerConfig
                 Contact = new OpenApiContact() { Name = "Matheus Fernandes", Email = "matheusfernandesofc@gmail.com" },
                 License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
             });
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                Name = "Authorization",
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
         });
 
         return builder;
@@ -24,7 +49,11 @@ public static class SwaggerConfig
     public static WebApplication UseSwaggerConfig(this WebApplication app)
     {
         app.UseSwagger();
-        app.UseSwaggerUI();
+        app.UseSwaggerUI(c =>
+        {
+            c.DisplayRequestDuration();
+            c.EnableFilter();
+        });
 
         return app;
     }
