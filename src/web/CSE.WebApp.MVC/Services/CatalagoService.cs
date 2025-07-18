f3c96c654dfa9400be30d9229c2f7fd208315d20
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-17 22:29:43 -0300

    feat: Implementa serviço de catálogo e interface correspondente
    Adicionadas novas importações nas classes `CatalogoService` e `ICatalogoService`.
    A classe `CatalogoService` foi implementada, herda de `Service` e implementa `ICatalogoService`, com um construtor que recebe `HttpClient` e `AppSettings`.

---
+++ src/web/CSE.WebApp.MVC/Services/CatalagoService.cs
@@ -0,0 +1,32 @@
+﻿using CSE.WebApp.MVC.Extensions;
+using CSE.WebApp.MVC.Models;
+using Microsoft.Extensions.Options;
+
+namespace CSE.WebApp.MVC.Services;
+
+public class CatalogoService(HttpClient httpClient,
+    IOptions<AppSettings> settings) : Service, ICatalogoService
+{
+    private readonly HttpClient _httpClient = httpClient;
+    private readonly AppSettings _settings = settings.Value;
+    private readonly string _catalogoUrl = settings.Value.CatalogoUrl;
+
+    public async Task<ProdutoViewModel> ObterPorId(Guid id)
+    {
+        var response = await _httpClient.GetAsync($"{_catalogoUrl}catalogo/produtos/{id}");
+
+        TratarErrosResponse(response);
+
+        return await DeserializarObjetoResponse<ProdutoViewModel>(response)
+               ?? throw new InvalidOperationException("Produto não encontrado.");
+    }
+
+    public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+    {
+        var response = await _httpClient.GetAsync($"{_catalogoUrl}catalogo/produtos/");
+
+        TratarErrosResponse(response);
+
+        return await DeserializarObjetoResponse<IEnumerable<ProdutoViewModel>>(response) ?? [];
+    }
+}
