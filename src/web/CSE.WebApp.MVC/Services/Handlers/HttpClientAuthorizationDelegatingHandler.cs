7b1d166a132fa3b3937c8b0bfe5559f61a51a9e7
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-21 20:58:03 -0300

    feat(HttpClientAuthorizationDelegatingHandler): Implementa manipulador de autorização para HttpClient
    Adiciona a classe `HttpClientAuthorizationDelegatingHandler` que herda de `DelegatingHandler`. Esta classe adiciona cabeçalhos de autorização a requisições HTTP, utilizando um objeto `IUser` para obter o contexto HTTP e o token do usuário.

---
+++ src/web/CSE.WebApp.MVC/Services/Handlers/HttpClientAuthorizationDelegatingHandler.cs
@@ -0,0 +1,28 @@
+﻿using CSE.WebApp.MVC.Extensions;
+using System.Net.Http.Headers;
+
+namespace CSE.WebApp.MVC.Services.Handlers;
+
+public class HttpClientAuthorizationDelegatingHandler(IUser user) : DelegatingHandler
+{
+    private readonly IUser _user = user;
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var authorizationHeader = _user.ObterHttpContext()?.Request.Headers.Authorization;
+
+        if (!string.IsNullOrEmpty(authorizationHeader))
+        {
+            request.Headers.Add("Authorization", [authorizationHeader]);
+        }
+
+        var token = _user.ObterUserToken();
+
+        if (token != null)
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+
+        return base.SendAsync(request, cancellationToken);
+    }
+}
