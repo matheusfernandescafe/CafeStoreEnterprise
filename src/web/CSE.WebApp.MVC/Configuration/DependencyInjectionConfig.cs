1657d44aa7dadc061c875ce7597a8c1b9a896791
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-22 22:13:51 -0300

    refactor(DI): Refatorar registro do ICatalogoService com Refit
    Alterado o registro do serviço `ICatalogoService` para utilizar um cliente nomeado "Refit", configurando a `BaseAddress` a partir da configuração `CatalogoUrl`. Adicionado o método `AddTypedClient` para registrar um cliente Refit para `ICatalogoServiceRefit`.

--- src/web/CSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/CSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -8,12 +8,20 @@ public static class DependencyInjectionConfig
 {
     public static WebApplicationBuilder RegisterServices(this WebApplicationBuilder builder)
     {
-        builder.Services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
+        var catalogoUrl = builder.Configuration.GetSection("CatalogoUrl").Value ?? "";
 
         builder.Services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
-        builder.Services.AddHttpClient<ICatalogoService, CatalogoService>()
-            .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
 
+        builder.Services.AddHttpClient("Refit", options =>
+        {
+            options.BaseAddress = new Uri(catalogoUrl);
+        })
+        .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+        .AddTypedClient(client => Refit.RestService.For<ICatalogoServiceRefit>(client));
+        //builder.Services.AddHttpClient<ICatalogoService, CatalogoService>()
+        //    .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
+
+        builder.Services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
         builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         builder.Services.AddScoped<IUser, AspNetUser>();
 
