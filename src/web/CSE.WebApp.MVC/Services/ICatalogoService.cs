017dac00a1f8a82aa8226caf57fc95c5a3f5b49f
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-22 22:12:59 -0300

    feat(ICatalogoService): Adicionar interface ICatalogoServiceRefit com Refit
    Implementada nova interface `ICatalogoServiceRefit` no arquivo `ICatalogoService.cs`, que inclui métodos para obter todos os produtos e um produto por ID, utilizando a biblioteca Refit para simplificar as chamadas HTTP.

--- src/web/CSE.WebApp.MVC/Services/ICatalogoService.cs
+++ src/web/CSE.WebApp.MVC/Services/ICatalogoService.cs
@@ -1,4 +1,5 @@
 ﻿using CSE.WebApp.MVC.Models;
+using Refit;
 
 namespace CSE.WebApp.MVC.Services;
 
@@ -6,4 +7,14 @@ public interface ICatalogoService
 {
     Task<IEnumerable<ProdutoViewModel>> ObterTodos();
     Task<ProdutoViewModel> ObterPorId(Guid id);
+}
+
+public interface ICatalogoServiceRefit
+{
+    [Get("/catalogo/produtos/")]
+    Task<IEnumerable<ProdutoViewModel>> ObterTodos();
+
+    [Get("/catalogo/produtos/{id}")]
+    Task<ProdutoViewModel> ObterPorId(Guid id);
+
 }
