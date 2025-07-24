5f9024009e8728e45224c4a1de7f68b9413b0c2c
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-23 21:42:01 -0300

    feat(DI): Adicionar Polly para resiliência em chamadas HTTP
    Implementadas políticas de resiliência usando Polly nas chamadas HTTP para `ICatalogoService`.

--- src/web/CSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/CSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,16 +1,35 @@
 ﻿using CSE.WebApp.MVC.Extensions;
 using CSE.WebApp.MVC.Services;
 using CSE.WebApp.MVC.Services.Handlers;
+using Polly;
 
 namespace CSE.WebApp.MVC.Configuration;
 
 public static class DependencyInjectionConfig
 {
     public static WebApplicationBuilder RegisterServices(this WebApplicationBuilder builder)
+    {        
+        builder.Services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
+
+        builder.Services.AddHttpClient<ICatalogoService, CatalogoService>()
+            .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+            //.AddTransientHttpErrorPolicy(
+            //p => p.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(600)));
+            .AddPolicyHandler(PollyExtensions.EsperarTentar())
+            .AddTransientHttpErrorPolicy(
+                p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30))
+            );
+
+        builder.Services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
+        builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        builder.Services.AddScoped<IUser, AspNetUser>();
+
+        return builder;
+    }
+
+    private static void AddRefit(this WebApplicationBuilder builder)
     {
         var catalogoUrl = builder.Configuration.GetSection("CatalogoUrl").Value ?? "";
-
-        builder.Services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
 
         builder.Services.AddHttpClient("Refit", options =>
         {
@@ -18,13 +37,5 @@ public static class DependencyInjectionConfig
         })
         .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
         .AddTypedClient(client => Refit.RestService.For<ICatalogoServiceRefit>(client));
-        //builder.Services.AddHttpClient<ICatalogoService, CatalogoService>()
-        //    .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
-
-        builder.Services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
-        builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-        builder.Services.AddScoped<IUser, AspNetUser>();
-
-        return builder;
     }
 }
