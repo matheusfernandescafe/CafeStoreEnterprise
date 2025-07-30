bd82c0ce97f00150317b16e5853d02a7f8707df6
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-29 22:27:34 -0300

    feat(Clientes): Criado arquivo e configurado injeção de dependências do projeto

---
+++ src/services/CSE.Cliente.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace CSE.Cliente.API.Configuration;
+
+public static class DependencyInjectionConfig
+{
+    public static WebApplicationBuilder AddDependecyInjectionConfig(this WebApplicationBuilder builder)
+    {
+        //builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+
+        return builder;
+    }
+}
