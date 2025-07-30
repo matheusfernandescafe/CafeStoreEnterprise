0988fdb7a4bbbe992c746744787dc9b8080abccf
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-29 22:25:52 -0300

    feat: Criado configuração base da API

---
+++ src/services/CSE.Cliente.API/Configuration/ApiConfig.cs
@@ -0,0 +1,39 @@
+﻿using CSE.WebAPI.Core.Identidade;
+
+namespace CSE.Cliente.API.Configuration;
+
+public static class ApiConfig
+{
+    public static WebApplicationBuilder AddApiConfig(this WebApplicationBuilder builder)
+    {
+        var environmentName = builder.Environment.EnvironmentName;
+
+        builder.Configuration
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.Configuration.AddUserSecrets<Program>();
+        }
+
+        builder.Services.AddControllers()
+            .ConfigureApiBehaviorOptions(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
+        return builder;
+    }
+
+    public static WebApplication UseApiConfig(this WebApplication app)
+    {
+        app.UseHttpsRedirection();
+        app.UseAuthConfiguration();
+        app.MapControllers();
+
+        return app;
+    }
+}
