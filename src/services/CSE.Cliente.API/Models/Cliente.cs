10964c85217616ec55b06b3adf4d4dcb39281122
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-30 19:56:00 -0300

    refactor(Clientes): Altera classe Cliente para utilizar as classes globais "Email" e "CPF"

--- src/services/CSE.Cliente.API/Models/Cliente.cs
+++ src/services/CSE.Cliente.API/Models/Cliente.cs
@@ -5,19 +5,29 @@ namespace CSE.Cliente.API.Models;
 public class Cliente : Entity, IAggregateRoot
 {
     public string Nome { get; private set; }
-    public string Email { get; private set; }
-    public string Cpf { get; private set; }
+    public Email Email { get; private set; }
+    public CPF Cpf { get; private set; }
     public bool Excluido { get; private set; }
     public Endereco Endereco { get; private set; }
 
     protected Cliente() { }
 
-    public Cliente(string nome, string email, string cpf)
+    public Cliente(Guid id, string nome, string email, string cpf)
     {
-        //Id = id;
+        Id = id;
         Nome = nome;
-        Email = email;
-        Cpf = cpf;
+        Email = new Email(email);
+        Cpf = new CPF(cpf);
         Excluido = false;
+    }
+
+    public void TrocarEmail(string email)
+    {
+        Email = new Email(email);
+    }
+
+    public void AtribuirEndereco(Endereco endereco)
+    {
+        Endereco = endereco;
     }
 }
