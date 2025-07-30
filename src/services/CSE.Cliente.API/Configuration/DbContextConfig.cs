32dae2f107fe9bc72a9dec05cc93769ad680d565
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-29 22:26:47 -0300

    feat(Clientes): Criado arquivos base de DbContext e configurado na API

---
+++ src/services/CSE.Cliente.API/Configuration/DbContextConfig.cs
@@ -0,0 +1,17 @@
+﻿using CSE.Cliente.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSE.Cliente.API.Configuration;
+
+public static class DbContextConfig
+{
+    public static WebApplicationBuilder AddDbContextConfig(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<ClienteContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        return builder;
+    }
+}
---
+++ src/services/CSE.Cliente.API/Data/ClienteContext.cs
@@ -0,0 +1,18 @@
+﻿using CSE.Core.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSE.Cliente.API.Data;
+
+public class ClienteContext(DbContextOptions<ClienteContext> options) : DbContext(options), IUnitOfWork
+{
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClienteContext).Assembly);
+    }
+
+    public async Task<bool> Commit()
+    {
+        return await base.SaveChangesAsync() > 0;
+    }
+}
