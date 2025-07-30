4abb32b14331c9d29b4ce4de90b9144eab8e0cd1
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-29 22:29:17 -0300

    feat(Clientes): Atualizado Program do projeto para adicionar e utilizar configurações da API

--- src/services/CSE.Cliente.API/Program.cs
+++ src/services/CSE.Cliente.API/Program.cs
@@ -1,25 +1,20 @@
+using CSE.Cliente.API.Configuration;
+using CSE.WebAPI.Core.Identidade;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder
+    .AddApiConfig()
+    .AddJwtConfiguration()
+    .AddDependecyInjectionConfig()
+    .AddCorsConfig()
+    .AddDbContextConfig()
+    .AddSwaggerConfig();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseSwaggerConfig()
+   .UseCorsConfig()
+   .UseApiConfig();
 
 app.Run();
