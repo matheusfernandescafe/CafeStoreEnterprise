1c1766235057ddace97ac7053b29c18ca0f2625f
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-29 22:26:16 -0300

    feat: Criado arquivos e configurado CORS da API

---
+++ src/services/CSE.Cliente.API/Configuration/CorsConfig.cs
@@ -0,0 +1,38 @@
+﻿namespace CSE.Cliente.API.Configuration;
+
+public static class CorsConfig
+{
+    public static WebApplicationBuilder AddCorsConfig(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("Development", builder =>
+                builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+
+            options.AddPolicy("Production", builder =>
+                builder
+                    .WithOrigins("https://localhost:9000")
+                    .WithMethods("POST")
+                    .AllowAnyHeader());
+        });
+
+        return builder;
+    }
+
+    public static WebApplication UseCorsConfig(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseCors("Development");
+        }
+        else
+        {
+            app.UseCors("Production");
+        }
+
+        return app;
+    }
+}
