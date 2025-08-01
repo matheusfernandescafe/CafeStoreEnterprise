a399ca0e33be23bb0b13f8b69baa487fce4e6d07
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2025-07-31 22:31:18 -0300

    feat(Context): Configura ChangeTracker do Context e adiciona as duas tabelas no contexto (Clientes e Enderecos)

--- src/services/CSE.Cliente.API/Data/ClienteContext.cs
+++ src/services/CSE.Cliente.API/Data/ClienteContext.cs
@@ -1,13 +1,29 @@
-﻿using CSE.Core.Data;
+﻿using CSE.Clientes.API.Models;
+using CSE.Core.Data;
 using Microsoft.EntityFrameworkCore;
 
-namespace CSE.Cliente.API.Data;
+namespace CSE.Clientes.API.Data;
 
-public class ClienteContext(DbContextOptions<ClienteContext> options) : DbContext(options), IUnitOfWork
+public class ClienteContext : DbContext, IUnitOfWork
 {
+    public ClienteContext(DbContextOptions<ClienteContext> options) : base(options)
+    {
+        ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        ChangeTracker.AutoDetectChangesEnabled = false;
+    }
+
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Endereco> Enderecos { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+            e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+            property.SetColumnType("varchar(100)");
+
+        foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+            .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClienteContext).Assembly);
     }
 
